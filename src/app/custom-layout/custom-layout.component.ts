88eeb171960206f5e1debc4d9bc089dae33a3d98
samueltutorreach <89348634+aksamuel66@users.noreply.github.com>
2023-11-24 22:11:44 +0530

    layout setup

--- src/app/custom-layout/custom-layout.component.ts
+++ src/app/custom-layout/custom-layout.component.ts
@@ -6,5 +6,137 @@ import { Component } from '@angular/core';
   styleUrls: ['./custom-layout.component.scss']
 })
 export class CustomLayoutComponent {
-
+  layoutData: any = [
+    {
+      id: 1,
+      columns: [
+        {
+          label: 12
+        }
+      ]
+    },
+    {
+      id: 2,
+      columns: [
+        {
+          label: 3
+        },
+        {
+          label: 9
+        }
+      ]
+    },
+    {
+      id: 3,
+      columns: [
+        {
+          label: 4
+        },
+        {
+          label: 8
+        }
+      ]
+    },
+    {
+      id: 4,
+      columns: [
+        {
+          label: 2
+        },
+        {
+          label: 5
+        },
+        {
+          label: 5
+        }
+      ]
+    },
+    {
+      id: 5,
+      columns: [
+        {
+          label: 2
+        },
+        {
+          label: 6
+        },
+        {
+          label: 4
+        }
+      ]
+    },
+    {
+      id: 6,
+      columns: [
+        {
+          label: 2
+        },
+        {
+          label: 8
+        },
+        {
+          label: 2
+        }
+      ]
+    },
+    {
+      id: 7,
+      columns: [
+        {
+          label: 3
+        },
+        {
+          label: 3
+        },
+        {
+          label: 3
+        },
+        {
+          label: 3
+        }
+      ]
+    },
+    {
+      id: 8,
+      columns: [
+        {
+          label: 2
+        },
+        {
+          label: 4
+        },
+        {
+          label: 4
+        },
+        {
+          label: 2
+        }
+      ]
+    },
+    {
+      id: 9,
+      columns: [
+        {
+          label: 4
+        },
+        {
+          label: 4
+        },
+        {
+          label: 4
+        }
+      ]
+    },
+    {
+      id: 10,
+      columns: [
+        {
+          label: 6
+        },
+        {
+          label: 6
+        }
+      ]
+    }
+  ];
 }
